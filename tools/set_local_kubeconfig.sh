13055c099330615df22bc235523b55f9a7f1b02e
Erwan SEITE <eseite@talend.com>
2020-02-04 11:53:23 +0100

    Add K8smaster management

--- tools/set_local_kubeconfig.sh
+++ tools/set_local_kubeconfig.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 KUBECONFIG=$(cd $(dirname $0)/..; pwd)/.kubeconfig
-vagrant ssh kube-master -c "sudo cat /etc/kubernetes/admin.conf" > ${KUBECONFIG}
-echo "export KUBECONFIG=${KUBECONFIG}"
+vagrant ssh kube-master -c "sudo cat /etc/kubernetes/admin.conf" 2>/dev/null > ${KUBECONFIG}
+IPADDR=$(vagrant ssh kube-master -c "ip addr show eth1 | grep 'inet ' | sed 's/  */ /g' | cut -d ' ' -f 3 | cut -d '/' -f 1" 2>/dev/null)
+echo "Please add or verify k8smaster in your /etc/hosts: $IPADDR"
+echo "Please do the following command on your laptop:"
+echo "  export KUBECONFIG=${KUBECONFIG}"
+
