9af14b919d0fe7729afdbd969cabad20ec84b837
Erwan SEITE <eseite@talend.com>
2020-09-21 11:41:19 +0200

    autoconfig

--- tools/set_local_kubeconfig.sh
+++ tools/set_local_kubeconfig.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 KUBECONFIG=$(cd $(dirname $0)/..; pwd)/.kubeconfig
 vagrant ssh kube-master -c "sudo cat /etc/kubernetes/admin.conf" 2>/dev/null > ${KUBECONFIG}
-IPADDR=$(vagrant ssh kube-master -c "ip addr show eth1 | grep 'inet ' | sed 's/  */ /g' | cut -d ' ' -f 3 | cut -d '/' -f 1" 2>/dev/null)
+IPADDR=$(vagrant ssh kube-master -c "ip addr show eth1 | grep 'inet ' | sed 's/  */ /g' | cut -d ' ' -f 3 | cut -d '/' -f 1" 2>/dev/null | sed 's/\r//g')
+if [ -n "${IPADDR}" ];
+then
+  grep -q ' k8smaster$' /etc/hosts
+  if [ $? -eq 0 ];
+  then
+    sudo sed -i "/ k8smaster$/c${IPADDR} k8smaster" /etc/hosts
+  else
+    sudo sed -i -e "$ a${IPADDR} k8smaster" /etc/hosts
+  fi
+fi
 echo "Please add or verify k8smaster in your /etc/hosts: $IPADDR"
+grep " k8smaster$" /etc/hosts
 echo "Please do the following command on your laptop:"
 echo "  export KUBECONFIG=${KUBECONFIG}"
 
